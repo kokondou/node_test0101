9ea9201cb61dceec5111f26b55c6d1140dccc328
kondou <kokondou@gmail.com>
2024-01-01 13:23:12 +0900

    first

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const activities = require("./activities.json");
+
+app.use(express.urlencoded({ extend: true }));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/autumn", function (req, res) {
+  fs.writeFile(__dirname + "/data.txt", req.body.activity, function () {
+    res.send("投稿完了");
+  });
+});
+
+app.post("/update", function (req, res) {
+  activities[0].activity = req.body.updActivity;
+  res.send(activities);
+});
+
+app.post("/delete", function (req, res) {
+  activities.splice(req.body.number, 1);
+  res.send(activities);
+});
+const port = process.env.port;
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
